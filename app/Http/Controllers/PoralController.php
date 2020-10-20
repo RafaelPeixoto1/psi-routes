5459ecd3a9fd269d66ebd4edc686332ad7ada425
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-20 13:29:34 +0100

    Update .editorconfig, .env.example, and 92 more files...

---
+++ app/Http/Controllers/PoralController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PoralController extends Controller
+{
+    //
+    public function index (){
+    	return view('entrada');
+
+    }
+    public function listarEquipas(){
+    	$teams = [
+    		'Desportivo das Aves',
+    		'Moreirense',
+    		'Famalicão',
+    	];
+    	return view('listar-equipas' ['equipas'=>$teams]);
+
+    }
+}
---
+++ resources/views/listar-equipas.blade.php
@@ -0,0 +1,6 @@
+<h1>Equipas</h1>
+@foreach($equipas as $equipa)
+<h2>{{$Equipa}}</h2>
+@endforeach
+
+<p>Está tudo</p>
